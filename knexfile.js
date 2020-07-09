af737c58a0b692d14325d0387e2b5ad33b0b1edf
Leonardo Garruba <leonardogarrubaaraujo@gamil.com>
2020-07-09 12:56:13 -0300

    Configurando a conexão para o ambiente de produção

--- knexfile.js
+++ knexfile.js
@@ -10,12 +10,23 @@ module.exports = {
       host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT
     },
 
     migrations : {
       directory: `${__dirname}/src/database/migrations`
     },
   },
+
+  production: {
+    client: 'pg',
+
+    connection: process.env.DATABASE_URL,
+
+    migrations: {
+      directory: `${__dirname}/src/database/migrations`
+    }
+  }
 
 };
