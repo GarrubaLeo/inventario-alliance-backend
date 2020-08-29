3d7befd67acdc57ee30dd8e1c0937106925d7793
Leonardo Garruba <leonardogarrubaaraujo@gamil.com>
2020-08-29 20:51:03 -0300

    Alteração na tabela computers

--- src/database/migrations/20200611182229_createTable_computers.js
+++ src/database/migrations/20200611182229_createTable_computers.js
@@ -1,18 +1,22 @@
 
 exports.up = function (knex) {
     return knex.schema.createTable('computers', (table) => {
+        //Informações de idenficação
         table.string('codigo').primary().notNullable();
+        table.string('torre').notNullable();
+        table.enu('status', ['S', 'N']).notNullable();
+        table.string('usuario')
+        table.string('obs').nullable();
+
+        //Ficha técnica
         table.string('ip').notNullable();
-        table.string('dominio').notNullable();
+        table.string('numSerie').notNullable();
         table.string('fabricante').notNullable();
         table.string('modelo').notNullable();
-        table.string('numSerie').notNullable();
         table.string('versaoOS').notNullable();
         table.string('chaveWindows').notNullable();
         table.string('versaoOffice').notNullable();
         table.string('chaveOffice').notNullable();
-        table.string('obs').nullable();
-        table.string('status').notNullable();
     });
 };
 
