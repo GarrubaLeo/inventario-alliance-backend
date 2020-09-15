9c1cf573cb83b571f90cb57f21fae5847b849f86
Leonardo Garruba <leonardogarrubaaraujo@gmail.com>
2020-09-15 11:48:09 -0300

    Adicionando middleware de autenticação

---
+++ src/middleware/Login.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports = (request, response, next) => {
+    try {
+        const token = request.headers.authorization.split(' ')[1];
+
+        const decode = jwt.verify(
+            token,
+            process.env.JWT_KEY
+        );
+
+        request.user = decode;
+
+        next();
+    } catch (error) {
+        return response.status(401).json({ error: 'Falha na autenticação!'})
+    }
+}
